e3212865e6db76869d362e69f0d3622e05cf5623
Emir Kabal <kabalemir@gmail.com>
2023-10-31 14:00:08 +0300

    added builder & bump version

---
+++ buildArchives.mjs
@@ -0,0 +1,48 @@
+import path from "node:path"
+import fs from "fs-extra"
+import archiver from "archiver"
+const dir = "build"
+
+const version = process.env.npm_package_version || "0.0.0"
+
+async function syncVersion() {
+  const manifest = await fs.readJson("./src/manifest.json")
+  manifest.version = version
+  await fs.writeJson("./src/manifest.json", manifest, { spaces: 2 })
+  console.info("Version synced ", version)
+}
+
+async function createArchive() {
+  const name = `${dir}/better-instagram-controls-${version}.zip`
+  const archive = archiver("zip", { zlib: { level: 9 } })
+  const stream = fs.createWriteStream(name)
+
+  archive.pipe(stream)
+
+  const entries = await fs.readdir("dist")
+  for (const entry of entries) {
+    archive.file(path.join("dist", entry), { name: entry })
+  }
+
+  console.info("Creating archive ", name)
+  await archive.finalize()
+}
+
+async function build() {
+  if (await fs.pathExists(dir)) {
+    await fs.remove(dir)
+  }
+  await fs.mkdir(dir)
+
+  await syncVersion()
+
+  try {
+    await createArchive()
+  } catch (error) {
+    console.error(error)
+  }
+
+  console.info("Build complete")
+}
+
+build()
